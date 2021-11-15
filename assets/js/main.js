350957aa5f5560ee273b0719f480036c7e2f5243
ilhambinyetov <binyetov631@gmail.com>
2021-11-15 18:34:25 +0400

    header and footer section created

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+
+    $('.search-button').click(function (e) {
+        e.preventDefault();
+        $('.search-box').show();
+
+
+    });
+
+
+
+    $('.basket-section').hover(
+        function () {
+            $(".cart-menu").show(1000);
+        },
+        // function () {
+        //     $(".cart-menu").hide();
+        // },
+    );
+
+    $(".hamburger-section").hover(
+        function () {
+            $(".user-menu").show(1000);
+        },
+        function () {
+            $(".user-menu").hide();
+        },
+
+    )
+
+});
