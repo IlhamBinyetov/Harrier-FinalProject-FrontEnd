9d346b3ea4678c29d1b18884cc502e91b77b3fae
ilhambinyetov <binyetov631@gmail.com>
2021-11-18 01:33:18 +0400

    slider section ended and select2 finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,5 +27,36 @@ $(document).ready(function () {
             $(".user-menu").hide();
         },
 
-    )
+    );
+
+    $('.sel-example').select2({
+
+    });
+
+    $('#sel2').select2({
+        disabled: true
+    });
+
+
+    //select part
+
+    var select = $('#sel2'),
+        cache = $('#sel2').clone();
+
+    $("#sel1").change(function () {
+        var id = $(this).val(),
+            options = [];
+        select.empty();
+        $("#sel2").select2('enable');
+        cache.find('option').each(function () {
+            if (id === '0') {
+                select.append($(this).clone());
+            } else if ($(this).data('val') == id) {
+                select.append($(this).clone());
+            }
+        });
+    });
+
+
 });
+
