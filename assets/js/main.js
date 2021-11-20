053b1bf2384211e6b5114621a721fef6dcb3ab11
ilhambinyetov <binyetov631@gmail.com>
2021-11-20 12:03:54 +0400

    finished the home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,6 +71,12 @@ $(document).ready(function () {
         slidesToScroll: 1,
         touchMove: false
     });
+    $('.deals-footer').slick({
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        touchMove: false
+    });
 
 
 
@@ -118,5 +124,35 @@ $(document).ready(function () {
 
 
 
+
+
+
 });
 
+//testimonial section
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
